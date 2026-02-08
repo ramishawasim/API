5b84cb8cd8ccb91f79473fdf53b239c475e022ff
ramishawasim <wasimramisha@gmail.com>
2026-02-07 23:50:40 -0500

    added genius api

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,1 +1,15 @@
 console.log("hello");
+
+const url = "https://api.genius.com/artists/16775?access_token=RiaYeZfmiPKD8eLqWk3fwhslwxxaEhgMAuFFyNCthW-Nwgyw70E6rRcZV4lc3Zhu";
+
+
+// console.log(url);
+
+async function getData(){
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log("Json data from API ", data);
+
+}
+
+getData();
