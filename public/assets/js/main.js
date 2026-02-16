064bd1736a3415cff0868211d03a5abdb55b1bb5
ramishawasim <wasimramisha@gmail.com>
2026-02-15 22:10:43 -0500

    added css

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,5 +1,4 @@
-console.log("hello");
-
+// testing API urls
 // const url = "https://api.genius.com/artists/16775?access_token=RiaYeZfmiPKD8eLqWk3fwhslwxxaEhgMAuFFyNCthW-Nwgyw70E6rRcZV4lc3Zhu";
 
 // const url = "https://api.genius.com/search?q=kendrick+lamar&access_token=RiaYeZfmiPKD8eLqWk3fwhslwxxaEhgMAuFFyNCthW-Nwgyw70E6rRcZV4lc3Zhu";
@@ -15,33 +14,42 @@ console.log(submitButton);
 const string = document.querySelector("#songs");
 
 
-
+// on click (submitting search function in html) prevent default and get input value
 submitButton.addEventListener("click", function(e){
     e.preventDefault();
 
+    // grab value from search bar
     const searchInput = document.getElementById("site-search");
     const searchArtist = searchInput.value;
+    // API access key
     const key = "RiaYeZfmiPKD8eLqWk3fwhslwxxaEhgMAuFFyNCthW-Nwgyw70E6rRcZV4lc3Zhu";
     console.log(searchArtist);
     
+    // call generate function to make the url and return it to next function
     const url = generateURL(searchArtist, key);
+    // start the function and pass it url
     getData(url);
 
 })
 
 
-
+// combine the variables to get the corresponding url
 function generateURL(artist, apiKey) {
     return `https://api.genius.com/search?q=${artist}&access_token=${apiKey}`;
 }
 
+// grab the data from the corresponding url
 async function getData(url){
     const response = await fetch(url);
     const data = await response.json();
-    // console.log("Json data from API ", data);
+    // grab the title of their most popular song
     const songs = data.response.hits[0].result.title;
+    // check if there's a null
+    if(data.response.hits[0]==null){
+        songs= "please check spelling";
+    }
+    // display the song
     console.log(songs);
-
     string.innerHTML = songs;
 }
 
